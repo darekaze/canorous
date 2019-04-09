1c594661b1f26ada069d2c19d052283ea01c0e85
darekaze <darekaze@gmail.com>
2019-04-09 17:55:13 +0800

    update root page

--- lib/app/views/frontpage/root.dart
+++ lib/app/views/frontpage/root.dart
@@ -1,90 +1,95 @@
-import 'package:flutter/material.dart';
 import 'package:canorous/app/views/frontpage/home.dart';
+import 'package:flutter/material.dart';
 
 class Root extends StatefulWidget {
   static const String PATH = '/';
-
-  Root({Key key}) : super(key: key);
+  const Root();
 
   @override
   _RootState createState() => _RootState();
 }
 
-class _RootState extends State<Root> {
-  PageController _pageController;
-  List<Widget> _pageList;
-  int _currentIndex = 0;
+class _RootState extends State<Root>
+    with SingleTickerProviderStateMixin {
+  static const List tabData = [
+    {'text': 'Home', 'icon': Icon(Icons.home)},
+    {'text': 'Search', 'icon': Icon(Icons.search)},
+    {'text': 'Library', 'icon': Icon(Icons.library_books)},
+    {'text': 'Moment', 'icon': Icon(Icons.music_video)}
+  ];
+
+  TabController _tabController;
+  List<Widget> myTabs = [];
+  String _appBarTitle = tabData[0]['text'];
+  int _currentTab = 0;
 
   @override
   void initState() {
     super.initState();
-    _pageController = PageController(
-      keepPage: true,
-      initialPage: 0,
+    _tabController = TabController(
+      length: 4,
+      vsync: this,
     );
-    _pageList = [
-      // Place the actual screen here (should be loaded async)
-      HomePage(),
-      Container(
-        color: Colors.red,
-      ),
-      Container(
-        color: Colors.purple,
-      ),
-      Container(
-        color: Colors.amber,
-      ),
-    ];
+    tabData.forEach((item) {
+      myTabs.add(Tab(
+        text: item['text'],
+        icon: item['icon'],
+      ));
+    });
+    _tabController.addListener(() {
+      print(_tabController.index);
+      // HACK: have delay in updating text
+      if (_tabController.index != _currentTab) {
+        _onTabChange();
+      }
+    });
   }
 
   @override
   void dispose() {
-    _pageController.dispose();
+    _tabController.dispose();
     super.dispose();
+  }
+
+  void _onTabChange() {
+    if (this.mounted) {
+      setState(() {
+        _appBarTitle = tabData[_tabController.index]['text'];
+        _currentTab = _tabController.index;
+      });
+    }
+  }
+
+  Widget _buildTabContent() {
+    return TabBarView(
+      controller: _tabController,
+      children: [
+        // TODO: implement other pages
+        HomePage(),
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.purple,
+        ),
+        Container(
+          color: Colors.amber,
+        ),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("App Name"),
+        title: Text(_appBarTitle),
       ),
-      body: PageView(
-        children: _pageList,
-        controller: _pageController,
-        onPageChanged: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            title: Text('Search'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.library_books),
-            title: Text('Library'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera),
-            title: Text('Moment'),
-          ),
-        ],
-        currentIndex: _currentIndex,
-        fixedColor: Colors.blue,
-        onTap: (int index) {
-          _currentIndex = index;
-          _pageController.jumpToPage(_currentIndex);
-        },
-      ),
+      body: _buildTabContent(),
+      bottomNavigationBar: TabBar(
+        controller: _tabController,
+        tabs: myTabs,
+      )
     );
   }
 }
