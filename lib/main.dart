706ca1d26009f4a87db12403bb33952d33b2d759
darekaze <darekaze@gmail.com>
2019-04-30 14:54:48 +0800

    add production server link

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() => Production();
 
 class Production extends Env {
   final String appName = "Canorous";
-  final String baseUrl = 'https://api.website.org'; // put sns server url
+  final String baseUrl = 'https://canorous.herokuapp.com/';
   EnvType environmentType = EnvType.PRODUCTION;
 
   final String dbName = 'canorous.db';
