ee05b75144ab60a72dcae3913d324dc34720131c
darekaze <darekaze@gmail.com>
2019-04-30 17:04:27 +0800

    Remove unecessary package

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -1,6 +1,5 @@
 import 'package:canorous/app/AppComponent.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_stetho/flutter_stetho.dart';
 import 'package:canorous/config/application.dart';
 
 enum EnvType {
@@ -23,9 +22,6 @@ class Env {
   }
 
   void _init() async {
-    if (envType != EnvType.PRODUCTION) {
-      Stetho.initialize();
-    }
     // Load application config
     var app = Application();
     await app.onCreate();
